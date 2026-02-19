0412fbbc3e758b87a0d255b6472d8e2f26dfb10a
limao <rafael.novack07@gmail.com>
2026-02-19 14:54:31 -0300

    trying to fix YCM22

--- appconfig/vim/install2.sh
+++ appconfig/vim/install2.sh
@@ -14,7 +14,7 @@ subinstall_params=""
 for param in "$@"
 do
   echo $param
-  if [ "$param" = "--unattended" ]; then
+  if [ $param="--unattended" ]; then
     echo "installing in unattended mode"
     unattended=1
     subinstall_params="--unattended"
@@ -46,42 +46,33 @@ while true; do
       sudo apt-get -y install libgnome2-dev libgnomeui-dev libbonoboui2-dev
     fi
 
-    # Dependências base
-    sudo apt-get -y install libncurses5-dev libncursesw5-dev libgtk2.0-dev libatk1.0-dev libcairo2-dev libx11-dev libxpm-dev libxt-dev clang-format build-essential cmake
-
-    # Instala o Python 3.12 no Jammy (22.04) para suportar o YCM novo
-    if [ -n "$JAMMY" ]; then
-      sudo add-apt-repository -y ppa:deadsnakes/ppa
-      sudo apt-get update
-      sudo apt-get -y install python3.12 python3.12-dev python3.12-venv python3-dev
-      PYTHON_CMD="python3.12"
-    else
-      sudo apt-get -y install python3-dev
-      PYTHON_CMD="python3"
-    fi
+    sudo apt-get -y install libncurses5-dev libgtk2.0-dev libatk1.0-dev libcairo2-dev libx11-dev libxpm-dev libxt-dev python3-dev clang-format
 
     sudo -H pip3 install rospkg
 
     # compile vim from sources
     cd $APP_PATH/../../submodules/vim
-    
-    # Atualiza o repositório para garantir Vim 9.1+
-    git checkout master || git checkout main
-    git pull
-
-    make clean || true
     ./configure --with-features=huge \
       --enable-multibyte \
       --enable-python3interp=yes \
-      --with-python3-command=$PYTHON_CMD \
+      --with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu \
       --enable-perlinterp=yes \
       --enable-luainterp=yes \
       --enable-gui=no \
       --enable-cscope --prefix=/usr
 
+      ## add for python2
+      # --enable-pythoninterp=yes \
+      # --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
+
+      ## add for python3
+      # --enable-python3interp=yes \
+      # --with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu \
+
       cd src
-      make -j$(nproc)
+      make
       cd ../
+      make VIMRUNTIMEDIR=/usr/share/vim/vim81
       sudo make install
 
     # set vim as a default git mergetool
@@ -93,12 +84,9 @@ while true; do
 
     # updated new plugins and clean old plugins
     if [ -n "$JAMMY" ]; then
-      # Garante que o arquivo da 22.04 seja o principal
-      ln -fs $APP_PATH/dotvim22rc ~/.vimrc
-      /usr/bin/vim -E -c "let g:user_mode=1" -c "PlugInstall" -c "wqa" || echo "It normally returns >0"
+      /usr/bin/vim -E -c "let g:user_mode=1" -c "so $APP_PATH/dotvim22rc" -c "PlugInstall" -c "wqa" || echo "It normally returns >0"
     else
-      ln -fs $APP_PATH/dotvimrc ~/.vimrc
-      /usr/bin/vim -E -c "let g:user_mode=1" -c "PlugInstall" -c "wqa" || echo "It normally returns >0"
+      /usr/bin/vim -E -c "let g:user_mode=1" -c "so $APP_PATH/dotvimrc" -c "PlugInstall" -c "wqa" || echo "It normally returns >0"
     fi
 
     if [ -n "$JAMMY" ]; then
@@ -130,31 +118,22 @@ while true; do
           # add llvm repo for clangd and python3-clang
           wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
           sudo apt-add-repository "deb http://apt.llvm.org/bionic/ llvm-toolchain-bionic-11 main"
+          # if 18.04, python3-clang has to be installed throught pip3 with prequisites manually from apt
           sudo apt-get -y install clang-11 libclang-11-dev
           sudo pip3 install clang
         else
-          # if 20.04/22.04, just install python3-clang from apt
+          # if 20.04, just install python3-clang from apt
           sudo apt-get -y install python3-clang 
         fi
-        
         # install prequisites for YCM
         sudo apt-get -y install clangd-11
         # set clangd to version 11 by default
-        sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-11 999 || true
+        sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-11 999
         sudo apt-get -y install libboost-all-dev
-
-        # Proteção: Se o PlugInstall não baixou o YCM, clona agora
-        if [ ! -d "$HOME/.vim/plugged/YouCompleteMe" ]; then
-          echo "Clonando YouCompleteMe manualmente..."
-          mkdir -p ~/.vim/plugged
-          git clone https://github.com/ycm-core/YouCompleteMe.git ~/.vim/plugged/YouCompleteMe
-        fi
 
         cd ~/.vim/plugged/YouCompleteMe/
         git submodule update --init --recursive
-        
-        # Usa a variável dinâmica para compilar com o Python correto
-        $PYTHON_CMD ./install.py --clangd-completer
+        python3 ./install.py --clangd-completer
 
         # link .ycm_extra_conf.py
         ln -fs $APP_PATH/dotycm_extra_conf.py ~/.ycm_extra_conf.py
