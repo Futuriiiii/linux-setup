207f7b4582a16a2edcab963b054714e967757637
limao <rafael.novack07@gmail.com>
2026-02-19 14:12:56 -0300

    trying to fix YCM

--- appconfig/vim/install.sh
+++ appconfig/vim/install.sh
@@ -1,76 +1,157 @@
 #!/bin/bash
+
 set -e
 
-echo "=== Installing latest Vim for YouCompleteMe (Ubuntu 22.04) ==="
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
 
-sudo apt update
+# get the path to this script
+APP_PATH=`dirname "$0"`
+APP_PATH=`( cd "$APP_PATH" && pwd )`
 
-# Remove old vim
-sudo apt remove -y vim vim-tiny vim-common vim-runtime || true
+unattended=0
+subinstall_params=""
+for param in "$@"
+do
+  echo $param
+  if [ $param="--unattended" ]; then
+    echo "installing in unattended mode"
+    unattended=1
+    subinstall_params="--unattended"
+  fi
+done
 
-# Install dependencies
-sudo apt install -y \
-  build-essential \
-  cmake \
-  git \
-  curl \
-  libncurses-dev \
-  libgtk-3-dev \
-  libatk1.0-dev \
-  libcairo2-dev \
-  libx11-dev \
-  libxpm-dev \
-  libxt-dev \
-  python3-dev \
-  python3-pip \
-  clang \
-  clangd \
-  libclang-dev \
-  libboost-all-dev
+var=`lsb_release -r | awk '{ print $2 }'`
+[ "$var" = "18.04" ] && export BEAVER=1
+[ "$var" = "22.04" ] && export JAMMY=1
 
-# Get python config dir automatically
-PYTHON_CONFIG_DIR=$(python3 - <<EOF
-import sysconfig
-print(sysconfig.get_config_var("LIBPL"))
-EOF
-)
+default=y
+while true; do
+  if [[ "$unattended" == "1" ]]
+  then
+    resp=$default
+  else
+    [[ -t 0 ]] && { read -t 10 -n 2 -p $'\e[1;32mInstall vim? [y/n] (default: '"$default"$')\e[0m\n' resp || resp=$default ; }
+  fi
+  response=`echo $resp | sed -r 's/(.*)$/\1=/'`
 
-echo "Python config dir: $PYTHON_CONFIG_DIR"
+  if [[ $response =~ ^(y|Y)=$ ]]
+  then
 
-# Clone latest Vim
-cd /tmp
-rm -rf vim
-git clone https://github.com/vim/vim.git
-cd vim
+    toilet Setting up vim
 
-# Compile Vim
-./configure \
-  --with-features=huge \
-  --enable-multibyte \
-  --enable-python3interp=yes \
-  --with-python3-config-dir=$PYTHON_CONFIG_DIR \
-  --enable-cscope \
-  --enable-terminal \
-  --enable-gui=no \
-  --prefix=/usr/local
+    sudo apt-get -y remove vim-* || echo ""
 
-make -j$(nproc)
-sudo make install
+    if [ -n "$BEAVER" ]; then
+      sudo apt-get -y install libgnome2-dev libgnomeui-dev libbonoboui2-dev
+    fi
 
-echo "Vim installed:"
-vim --version | grep python
+    sudo apt-get -y install libncurses5-dev libgtk2.0-dev libatk1.0-dev libcairo2-dev libx11-dev libxpm-dev libxt-dev python3-dev clang-format
 
-# Install plugins
-echo "Installing plugins..."
-vim +PlugInstall +qall || true
+    sudo -H pip3 install rospkg
 
-# Compile YouCompleteMe
-if [ -d "$HOME/.vim/plugged/YouCompleteMe" ]; then
-  cd ~/.vim/plugged/YouCompleteMe
-  git submodule update --init --recursive
-  python3 install.py --clangd-completer
-else
-  echo "YouCompleteMe not found in ~/.vim/plugged"
-fi
+    # compile vim from sources
+    cd $APP_PATH/../../submodules/vim
+    ./configure --with-features=huge \
+      --enable-multibyte \
+      --enable-python3interp=yes \
+      --with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu \
+      --enable-perlinterp=yes \
+      --enable-luainterp=yes \
+      --enable-gui=no \
+      --enable-cscope --prefix=/usr
 
-echo "=== DONE ==="
+      ## add for python2
+      # --enable-pythoninterp=yes \
+      # --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
+
+      ## add for python3
+      # --enable-python3interp=yes \
+      # --with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu \
+
+      cd src
+      make
+      cd ../
+      make VIMRUNTIMEDIR=/usr/share/vim/vim81
+      sudo make install
+
+    # set vim as a default git mergetool
+    git config --global merge.tool vimdiff
+
+    # symlink vim settings
+    rm -rf ~/.vim
+    ln -fs $APP_PATH/dotvim ~/.vim
+
+    # updated new plugins and clean old plugins
+    if [ -n "$JAMMY" ]; then
+      /usr/bin/vim -E -c "let g:user_mode=1" -c "so $APP_PATH/dotvim22rc" -c "PlugInstall" -c "wqa" || echo "It normally returns >0"
+    else
+      /usr/bin/vim -E -c "let g:user_mode=1" -c "so $APP_PATH/dotvimrc" -c "PlugInstall" -c "wqa" || echo "It normally returns >0"
+    fi
+
+    if [ -n "$JAMMY" ]; then
+      default=n
+    else
+      default=y
+    fi
+
+    while true; do
+      if [[ "$unattended" == "1" ]]
+      then
+        resp=$default
+      else
+        [[ -t 0 ]] && { read -t 10 -n 2 -p $'\e[1;32mCompile YouCompleteMe? [y/n] (default: '"$default"$')\e[0m\n' resp || resp=$default ; }
+      fi
+      response=`echo $resp | sed -r 's/(.*)$/\1=/'`
+
+      if [[ $response =~ ^(y|Y)=$ ]]
+      then
+
+        # set youcompleteme
+        toilet Setting up youcompleteme
+
+        # if not on 20.04, g++-8 has to be installed manually
+        if [ -n "$BEAVER" ]; then
+          sudo apt-get -y install g++-8
+          sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 700 --slave /usr/bin/g++ g++ /usr/bin/g++-7
+          sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 800 --slave /usr/bin/g++ g++ /usr/bin/g++-8
+          # add llvm repo for clangd and python3-clang
+          wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
+          sudo apt-add-repository "deb http://apt.llvm.org/bionic/ llvm-toolchain-bionic-11 main"
+          # if 18.04, python3-clang has to be installed throught pip3 with prequisites manually from apt
+          sudo apt-get -y install clang-11 libclang-11-dev
+          sudo pip3 install clang
+        else
+          # if 20.04, just install python3-clang from apt
+          sudo apt-get -y install python3-clang 
+        fi
+        # install prequisites for YCM
+        sudo apt-get -y install clangd-11
+        # set clangd to version 11 by default
+        sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-11 999
+        sudo apt-get -y install libboost-all-dev
+
+        cd ~/.vim/plugged/YouCompleteMe/
+        git submodule update --init --recursive
+        python3 ./install.py --clangd-completer
+
+        # link .ycm_extra_conf.py
+        ln -fs $APP_PATH/dotycm_extra_conf.py ~/.ycm_extra_conf.py
+
+        break
+      elif [[ $response =~ ^(n|N)=$ ]]
+      then
+        break
+      else
+        echo " What? \"$resp\" is not a correct answer. Try y+Enter."
+      fi
+    done
+
+    break
+  elif [[ $response =~ ^(n|N)=$ ]]
+  then
+    break
+  else
+    echo " What? \"$resp\" is not a correct answer. Try y+Enter."
+  fi
+done
