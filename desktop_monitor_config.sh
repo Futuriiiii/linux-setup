b32b1d2a3130854e521fb67c79f5945ea26c283a
rafael <rafael.novack07@gmail.com>
2026-02-19 22:31:21 -0300

    update desktop monitor config

--- desktop_monitor_config.sh
+++ desktop_monitor_config.sh
@@ -15,10 +15,38 @@ MODE2="1920x1080"
 RATE2="144"
 # ===========================
 
-NEW_LINES=$(cat <<EOF
+XRANDR_LINES=$(cat <<EOF
 
 exec --no-startup-id xrandr --output $OUTPUT2 --mode $MODE2 --rate $RATE2 
 exec --no-startup-id xrandr --output $OUTPUT1 --mode $MODE1 --rate $RATE1 --right-of $OUTPUT2
+EOF
+)
+
+OLD_WORKSPACES=$(cat <<'EOF'
+set $workspace1 "1: "
+set $workspace2 "2: "
+set $workspace3 "3: "
+set $workspace4 "4: "
+set $workspace5 "5"
+set $workspace6 "6"
+set $workspace7 "7"
+set $workspace8 "8"
+set $workspace9 "9"
+set $workspace10 "10: "
+EOF
+)
+
+NEW_WORKSPACES=$(cat <<'EOF'
+set $workspace1 "1: "
+set $workspace2 "2"
+set $workspace3 "3"
+set $workspace4 "4"
+set $workspace5 "5"
+set $workspace6 "6"
+set $workspace7 "7: "
+set $workspace8 "8: "
+set $workspace9 "9: "
+set $workspace10 "10: "
 EOF
 )
 
@@ -38,11 +66,40 @@ if grep -Fq "$OUTPUT1 --mode $MODE1 --rate $RATE1" "$CONFIG"; then
     exit 0
 fi
 
-awk -v target="$TARGET_LINE" -v newline="$NEW_LINES" '
+awk -v target="$TARGET_LINE" -v newline="$XRANDR_LINES" '
 {
     print
     if ($0 == target) {
         print newline
+    }
+}
+
+' "$CONFIG" > "$CONFIG.tmp" && mv "$CONFIG.tmp" "$CONFIG"
+
+awk -v old="$OLD_WORKSPACES" -v new="$NEW_WORKSPACES" '
+BEGIN {
+    split(old, o, "\n")
+    old_len = length(o)
+}
+{
+    buffer[NR] = $0
+}
+END {
+    for (i = 1; i <= NR; i++) {
+        match_block = 1
+        for (j = 1; j <= old_len; j++) {
+            if (buffer[i+j-1] != o[j]) {
+                match_block = 0
+                break
+            }
+        }
+
+        if (match_block) {
+            print new
+            i += old_len - 1
+        } else {
+            print buffer[i]
+        }
     }
 }
 ' "$CONFIG" > "$CONFIG.tmp" && mv "$CONFIG.tmp" "$CONFIG"
